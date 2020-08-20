d2bcb7ae95c53b6b26b46a5ed5646f7ef4b18c59
Thiego Barros <thiegobarros@gmail.com>
2020-08-19 22:32:10 -0400

    migrations

---
+++ database/migrations/2020_08_20_022146_create_sales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('fruit_id');
+            $table->integer('quantidade_fruta');
+            $table->decimal('valor', 8, 2);
+            $table->timestamps();
+
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('fruit_id')->references('id')->on('fruits');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
